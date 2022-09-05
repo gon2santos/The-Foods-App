9cd099852419a72c5566bac7617c950e82c03068
gon2santos <necchigonzalo@gmail.com>
2022-09-05 08:45:20 -0300

    completed landing page

--- client/src/containers/Init.jsx
+++ client/src/containers/Init.jsx
@@ -1,12 +1,27 @@
-import './Init.css'; 
 import React from 'react';
 import { Link } from "react-router-dom";
+import plate from '../images/plate.jpeg';
+import logo from '../images/logo.png';
+import s from './Init.module.css';
+
+const spanStyle = {
+  color: 'coral',
+  position: 'absolute',
+  right: '5%',
+  fontSize: '12px'
+}
 
 function Init() {
   return (
-    <div className="Init">
-      <h1>Init component!</h1>
-      <button><Link to="/home">Home</Link></button>
+    <div>
+      <div className={s.div}>
+        <img src={plate} alt="plate" width="612" height="612" className={s.image} />
+        <img src={logo} alt="logo" width="727" height="214" className={s.logo} />
+        <div className={s.divButton}>
+          <button className={s.button}><Link to="/home" className={s.linkStyle}>Start</Link></button>
+        </div>
+      </div>
+      <span class="Init_txt__3lp-9" style={spanStyle}>by Gonzalo Dos Santos Necchi</span>
     </div>
   );
 }
