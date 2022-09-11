b7a52d6e609775a4eca813cab7f6338fb48d3711
gon2santos <necchigonzalo@gmail.com>
2022-09-11 09:39:10 -0300

    Implemented filter by special diets (keto, primal, paleo, etc.)

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -19,6 +19,14 @@ export default function RecipeList() {
     let vegan = false;
     let vegetarian = false;
     let glutenFree = false;
+    let Ketogenic = false;
+    let LactoOvoVegetarian = false;
+    let Pescetarian = false;
+    let Paleo = false;
+    let Primal = false;
+    let LowFODMAP = false;
+    let Whole30 = false;
+    let DairyFree = false;
 
     useEffect(() => { setRendered_recipes(store_recipes) }, [store_recipes]);
 
@@ -74,19 +82,7 @@ export default function RecipeList() {
         setUpdateList(!updateList);
     };
 
-    const filterList = (e, diet) => { //FILTER FUNCTION V1
-        if(diet === 'vegetarian') vegetarian = e.target.checked;             
-        else if(diet === 'vegan') vegan = e.target.checked;        
-        else if(diet === 'glutenFree') glutenFree = e.target.checked;
-
-        setRendered_recipes(store_recipes);
-        
-        if(vegetarian) setRendered_recipes(rendered_recipes.filter(item => item.vegetarian === vegetarian));
-        if(vegan) setRendered_recipes(rendered_recipes.filter(item => item.vegan === vegan));
-        if(glutenFree) setRendered_recipes(rendered_recipes.filter(item => item.glutenFree === glutenFree));
-    }
-
-    /*const [sta_vegan, setVegan] = useState(false);
+     /*const [sta_vegan, setVegan] = useState(false);
     const [sta_vegetarian, setVegetarian] = useState(false);
     const [sta_glutenFree, setGlutenFree] = useState(false);
 
@@ -129,6 +125,54 @@ export default function RecipeList() {
         console.log(`glutenFree checked: ${glutenFree}`);
     } */
 
+    const filterList = (e, diet) => { //FILTER FUNCTION V1
+        if(diet === 'vegetarian') vegetarian = e.target.checked;             
+        else if(diet === 'vegan') vegan = e.target.checked;        
+        else if(diet === 'glutenFree') glutenFree = e.target.checked;
+        else if(diet === 'Ketogenic') Ketogenic = e.target.checked; 
+        else if(diet === 'LactoOvoVegetarian') LactoOvoVegetarian = e.target.checked; 
+        else if(diet === 'Pescetarian') Pescetarian = e.target.checked; 
+        else if(diet === 'Paleo') Paleo = e.target.checked; 
+        else if(diet === 'Primal') Primal = e.target.checked; 
+        else if(diet === 'LowFODMAP') LowFODMAP = e.target.checked; 
+        else if(diet === 'Whole30') Whole30 = e.target.checked; 
+        else if(diet === 'DairyFree') DairyFree = e.target.checked; 
+
+        setRendered_recipes(store_recipes);
+        
+        if(vegetarian) setRendered_recipes(rendered_recipes.filter(item => item.vegetarian === vegetarian));
+        if(vegan) setRendered_recipes(rendered_recipes.filter(item => item.vegan === vegan));
+        if(glutenFree) setRendered_recipes(rendered_recipes.filter(item => item.glutenFree === glutenFree));
+        if(Paleo) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("paleolithic", 0)));
+        };
+        if(Pescetarian) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("pescatarian", 0)));
+        };
+        /* if(Pescetarian) {
+            rendered_recipes.forEach(element => console.log(element.diets));
+            //setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("pescatarian", 0)));
+        }; */
+        if(Ketogenic) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("ketogenic", 0)));
+        };
+        if(LactoOvoVegetarian) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("lacto ovo vegetarian", 0)));
+        };
+        if(Primal) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("primal", 0)));
+        };
+        if(LowFODMAP) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("low FODMAP", 0)));
+        };
+        if(Whole30) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("whole 30", 0)));
+        };
+        if(DairyFree) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("dairy free", 0)));
+        };
+    }
+
     const changeStyle = () => { ordStyle === s.asc_order_button ? setOrdStyle(s.desc_order_button) : setOrdStyle(s.asc_order_button); }
 
     return (
@@ -141,6 +185,14 @@ export default function RecipeList() {
                                 <span>Vegetarian</span><input type="checkbox" name="vegetarian" onChange={e => filterList(e, 'vegetarian')} />
                                 <span>Vegan</span><input type="checkbox" name="vegan" onChange={e => filterList(e, 'vegan')} />
                                 <span>Gluten Free</span><input type="checkbox" name="glutenFree" onChange={e => filterList(e, 'glutenFree')} />
+                                <span>Ketogenic</span><input type="checkbox" name="Ketogenic" onChange={e => filterList(e, 'Ketogenic')} />
+                                <span>Lacto Ovo Vegetarian</span><input type="checkbox" name="LactoVegetarian" onChange={e => filterList(e, 'LactoOvoVegetarian')} />
+                                <span>Pescetarian</span><input type="checkbox" name="Pescetarian" onChange={e => filterList(e, 'Pescetarian')} />
+                                <span>Paleo</span><input type="checkbox" name="Paleo" onChange={e => filterList(e, 'Paleo')} />
+                                <span>Primal</span><input type="checkbox" name="Primal" onChange={e => filterList(e, 'Primal')} />
+                                <span>Low FODMAP</span><input type="checkbox" name="LowFODMAP" onChange={e => filterList(e, 'LowFODMAP')} />
+                                <span>Whole30</span><input type="checkbox" name="Whole30" onChange={e => filterList(e, 'Whole30')} />
+
                             </div>
 
                         </div>
