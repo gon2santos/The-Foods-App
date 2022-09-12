9e6de88da19e4119fffdd811144c3e4df301e281
gon2santos <necchigonzalo@gmail.com>
2022-09-12 12:20:02 -0300

    Styles recipeList button, grouped filter checkboxes inside one filter button

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -15,6 +15,7 @@ export default function RecipeList() {
     const [rcpIdx, setRcpIdx] = useState(0);
     const [count, setCount] = useState(0);
     const [ordStyle, setOrdStyle] = useState(s.desc_order_button);
+    const [accStyle, setAccStyle] = useState(s.inactive);
 
     let vegan = false;
     let vegetarian = false;
@@ -35,13 +36,13 @@ export default function RecipeList() {
             setRcpIdx(rcpIdx + 9);
             setCount(count + 1);
         }
-    }
+    };
     const pgPrev = function () {
         if (rcpIdx > 8) {
             setRcpIdx(rcpIdx - 9);
             setCount(count - 1);
         }
-    }
+    };
 
     const sortListName = () => {
         rendered_recipes.sort((a, b) => {
@@ -82,128 +83,92 @@ export default function RecipeList() {
         setUpdateList(!updateList);
     };
 
-     /*const [sta_vegan, setVegan] = useState(false);
-    const [sta_vegetarian, setVegetarian] = useState(false);
-    const [sta_glutenFree, setGlutenFree] = useState(false);
-
-    const filterList = (e, diet) => { //FILTER FUNCTION V2
-        vegan = sta_vegan;
-        vegetarian = sta_vegetarian;
-        glutenFree = sta_glutenFree;
-
-        if (diet === 'vegetarian') {
-            vegetarian = e.target.checked;
-            setVegetarian(vegetarian);
-        }
-
-        if (diet === 'vegan') {
-            vegan = e.target.checked;
-            setVegan(vegan);
-        }
-
-        if (diet === 'glutenFree') {
-            glutenFree = e.target.checked;
-            setGlutenFree(glutenFree);
-        }
-        
-        let auxRecipes = store_recipes;
-
-        auxRecipes = store_recipes.filter(item => {
-            let ret = false;
-            if(item.vegetarian === vegetarian){
-                if(item.vegan === vegan){
-                    if(item.glutenFree === glutenFree){
-                        ret = true;
-                    }}}
-                    return ret;
-        });
-
-        setRendered_recipes(auxRecipes);
-
-        console.log(`vegan checked: ${vegan}`);
-        console.log(`vegetarian checked: ${vegetarian}`);
-        console.log(`glutenFree checked: ${glutenFree}`);
-    } */
-
     const filterList = (e, diet) => { //FILTER FUNCTION V1
-        if(diet === 'vegetarian') vegetarian = e.target.checked;             
-        else if(diet === 'vegan') vegan = e.target.checked;        
-        else if(diet === 'glutenFree') glutenFree = e.target.checked;
-        else if(diet === 'Ketogenic') Ketogenic = e.target.checked; 
-        else if(diet === 'LactoOvoVegetarian') LactoOvoVegetarian = e.target.checked; 
-        else if(diet === 'Pescetarian') Pescetarian = e.target.checked; 
-        else if(diet === 'Paleo') Paleo = e.target.checked; 
-        else if(diet === 'Primal') Primal = e.target.checked; 
-        else if(diet === 'LowFODMAP') LowFODMAP = e.target.checked; 
-        else if(diet === 'Whole30') Whole30 = e.target.checked; 
-        else if(diet === 'DairyFree') DairyFree = e.target.checked; 
+        if (diet === 'vegetarian') vegetarian = e.target.checked;
+        else if (diet === 'vegan') vegan = e.target.checked;
+        else if (diet === 'glutenFree') glutenFree = e.target.checked;
+        else if (diet === 'Ketogenic') Ketogenic = e.target.checked;
+        else if (diet === 'LactoOvoVegetarian') LactoOvoVegetarian = e.target.checked;
+        else if (diet === 'Pescetarian') Pescetarian = e.target.checked;
+        else if (diet === 'Paleo') Paleo = e.target.checked;
+        else if (diet === 'Primal') Primal = e.target.checked;
+        else if (diet === 'LowFODMAP') LowFODMAP = e.target.checked;
+        else if (diet === 'Whole30') Whole30 = e.target.checked;
+        else if (diet === 'DairyFree') DairyFree = e.target.checked;
 
         setRendered_recipes(store_recipes);
-        
-        if(vegetarian) setRendered_recipes(rendered_recipes.filter(item => item.vegetarian === vegetarian));
-        if(vegan) setRendered_recipes(rendered_recipes.filter(item => item.vegan === vegan));
-        if(glutenFree) setRendered_recipes(rendered_recipes.filter(item => item.glutenFree === glutenFree));
-        if(Paleo) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("paleolithic", 0)));
-        };
-        if(Pescetarian) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("pescatarian", 0)));
-        };
-        /* if(Pescetarian) {
-            rendered_recipes.forEach(element => console.log(element.diets));
-            //setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("pescatarian", 0)));
-        }; */
-        if(Ketogenic) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("ketogenic", 0)));
-        };
-        if(LactoOvoVegetarian) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("lacto ovo vegetarian", 0)));
-        };
-        if(Primal) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("primal", 0)));
-        };
-        if(LowFODMAP) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("low FODMAP", 0)));
-        };
-        if(Whole30) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("whole 30", 0)));
-        };
-        if(DairyFree) {
-            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("dairy free", 0)));
-        };
-    }
 
-    const changeStyle = () => { ordStyle === s.asc_order_button ? setOrdStyle(s.desc_order_button) : setOrdStyle(s.asc_order_button); }
+        if (vegetarian) setRendered_recipes(rendered_recipes.filter(item => item.vegetarian === vegetarian));
+        if (vegan) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("vegan", 0)));
+        }
+        if (glutenFree) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("gluten free", 0)));
+        }
+        if (Paleo) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("paleolithic", 0)));
+        }
+        if (Pescetarian) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("pescatarian", 0)));
+        }
+        if (Ketogenic) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("ketogenic", 0)));
+        }
+        if (LactoOvoVegetarian) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("lacto ovo vegetarian", 0)));
+        }
+        if (Primal) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("primal", 0)));
+        }
+        if (LowFODMAP) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("low FODMAP", 0)));
+        }
+        if (Whole30) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("whole 30", 0)));
+        }
+        if (DairyFree) {
+            setRendered_recipes(rendered_recipes.filter(element => element.diets.includes("dairy free", 0)));
+        }
+
+        setAccStyle(s.inactive);
+    };
+
+    const handleAccordionButton = () => {
+        changeAccStyle();
+    };
+
+    const changeOrdStyle = () => { ordStyle === s.asc_order_button ? setOrdStyle(s.desc_order_button) : setOrdStyle(s.asc_order_button); };
+    const changeAccStyle = () => { accStyle === s.active ? setAccStyle(s.inactive) : setAccStyle(s.active); };
 
     return (
         <div className={s.body}>
             <div>
                 <ul className={s.ul}>
                     <div className={s.nav_contain}>
-                        <div>
-                            <div>
-                                <span>Vegetarian</span><input type="checkbox" name="vegetarian" onChange={e => filterList(e, 'vegetarian')} />
-                                <span>Vegan</span><input type="checkbox" name="vegan" onChange={e => filterList(e, 'vegan')} />
-                                <span>Gluten Free</span><input type="checkbox" name="glutenFree" onChange={e => filterList(e, 'glutenFree')} />
-                                <span>Ketogenic</span><input type="checkbox" name="Ketogenic" onChange={e => filterList(e, 'Ketogenic')} />
-                                <span>Lacto Ovo Vegetarian</span><input type="checkbox" name="LactoVegetarian" onChange={e => filterList(e, 'LactoOvoVegetarian')} />
-                                <span>Pescetarian</span><input type="checkbox" name="Pescetarian" onChange={e => filterList(e, 'Pescetarian')} />
-                                <span>Paleo</span><input type="checkbox" name="Paleo" onChange={e => filterList(e, 'Paleo')} />
-                                <span>Primal</span><input type="checkbox" name="Primal" onChange={e => filterList(e, 'Primal')} />
-                                <span>Low FODMAP</span><input type="checkbox" name="LowFODMAP" onChange={e => filterList(e, 'LowFODMAP')} />
-                                <span>Whole30</span><input type="checkbox" name="Whole30" onChange={e => filterList(e, 'Whole30')} />
-
+                        <div className={s.buttonsOrder}>
+                            <div><button className={s.headButtons} onClick={handleAccordionButton}>Sort by diet</button>
+                                <div className={accStyle}>
+                                    <div><input type="checkbox" id="vegetarian" onChange={e => filterList(e, 'vegetarian')} /><label for="vegetarian">Vegetarian</label></div>
+                                    <div><input type="checkbox" id="vegan" onChange={e => filterList(e, 'vegan')} /><label for="vegan">Vegan</label></div>
+                                    <div><input type="checkbox" id="glutenFree" onChange={e => filterList(e, 'glutenFree')} /><label for="glutenFree">Gluten Flabel</label></div>
+                                    <div><input type="checkbox" id="Ketogenic" onChange={e => filterList(e, 'Ketogenic')} /><label for="Ketogenic">Ketogenic</label></div>
+                                    <div><input type="checkbox" id="LactoVegetarian" onChange={e => filterList(e, 'LactoOvoVegetarian')} /><label for="LactoVegetarian">Lacto-Olabel Veg</label></div>
+                                    <div><input type="checkbox" id="Pescetarian" onChange={e => filterList(e, 'Pescetarian')} /><label for="Pescetarian">Pescetarian</label></div>
+                                    <div><input type="checkbox" id="Paleo" onChange={e => filterList(e, 'Paleo')} /><label for="Paleo">Paleo</label></div>
+                                    <div><input type="checkbox" id="Primal" onChange={e => filterList(e, 'Primal')} /><label for="Primal">Primal</label></div>
+                                    <div><input type="checkbox" id="LowFODMAP" onChange={e => filterList(e, 'LowFODMAP')} /><label for="LowFODMAP">Low FODMAP</label></div>
+                                    <div><input type="checkbox" id="Whole30" onChange={e => filterList(e, 'Whole30')} /><label for="Whole30">Whole30</label></div>
+                                </div></div>
+                            <button onClick={sortListName} className={s.headButtons}>Sort by name</button>
+                            <button onClick={sortListHS} className={s.headButtons}>Sort by Health Score</button>
+                            <button onClick={changeOrdStyle} className={ordStyle}>&gt;</button>
+                            <div className={s.div_navBar}>
+                                <button className={s.nav_button} onClick={pgPrev}>&lt;</button>
+                                <span>{count}/{Math.ceil((rendered_recipes?.length / 9) - 1)}</span>
+                                <button className={s.nav_button} onClick={pgNext}>&gt;</button>
                             </div>
+                        </div>
 
-                        </div>
-                        <button onClick={sortListName}>Sort by name</button>
-                        <button onClick={sortListHS}>Sort by Health Score</button>
-                        <button onClick={changeStyle} className={ordStyle}>&gt;</button>
-                        <div className={s.div_navBar}>
-                            <button className={s.nav_button} onClick={pgPrev}>&lt;</button>
-                            <span>{count}/{Math.ceil((rendered_recipes?.length / 9) - 1)}</span>
-                            <button className={s.nav_button} onClick={pgNext}>&gt;</button>
-                        </div>
                     </div>
                     {rendered_recipes?.slice(rcpIdx, (rcpIdx + 9)).map((r) =>
                         <li key={r.id} className={s.li}><img src={r.image} className={s.image} alt="recipePicture" /><Link to={`recipe/${r.id}/detail`} onClick={() => dispatch(toggleView(false))} className={s.link_component}>{r.title}</Link></li>
