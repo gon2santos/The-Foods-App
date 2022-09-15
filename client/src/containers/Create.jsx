bb2df9bdbd5203fc4fbe1e4c54866a038213522e
gon2santos <necchigonzalo@gmail.com>
2022-09-14 21:11:01 -0300

    Added diets to create new recipe form

--- client/src/containers/Create.jsx
+++ client/src/containers/Create.jsx
@@ -9,69 +9,75 @@ function Create() {
   const [summary, setSummary] = useState('');
   const [hs, setHs] = useState(0);
   const [sbs, setSbs] = useState('');
-  const [vegetarian, setVegetarian] = useState(false);
-  const [vegan, setVegan] = useState(false);
-  const [glutenFree, setGlutenFree] = useState(false);
-  const [dairyFree, setDairyFree] = useState(false);
+
+  const [Vegetarian, setVegetarian] = useState(false);
+  const [Vegan, setVegan] = useState(false);
+  const [GlutenFree, setGlutenFree] = useState(false);
+  const [DairyFree, setDairyFree] = useState(false);
+  const [Ketogenic, setKetogenic] = useState(false);
+  const [LactoOvoVegetarian, setLactoOvoVegetarian] = useState(false);
+  const [Pescetarian, setPescetarian] = useState(false);
+  const [Paleo, setPaleo] = useState(false);
+  const [Primal, setPrimal] = useState(false);
+  const [LowFODMAP, setLowFODMAP] = useState(false);
+  const [Whole30, setWhole30] = useState(false);
+
+
   const [enableBtn, setEnableBtn] = useState(true);
   const [warningName, setWarningName] = useState('*');
   const [warningSummary, setWarningSummary] = useState('*');
   const [warningHS, setWarningHS] = useState('*');
   const [warningSbs, setWarningSbs] = useState('*');
+  const [warningForbiddenWords, setWarningForbiddenWords] = useState('');
+  const [hidden, setHidden] = useState(s.hide);
 
   function handleNameChange(e) {
     const regex = /^[\w- ]{4,}$/gm;
-    if(regex.test(e.target.value)){
+    if (regex.test(e.target.value)) {
       setName(e.target.value);
       setWarningName('');
     }
-    else{
+    else {
       setWarningName('Name has to be at least 4 characters long and cannot contain special characters.');
     }
   }
   function handleSummaryChange(e) {
     const regex = /^[\w-\.\, ]{50,}$/gm;
-    if(regex.test(e.target.value)){
+    if (regex.test(e.target.value)) {
       setSummary(e.target.value);
       setWarningSummary('');
     }
-    else{
+    else {
       setWarningSummary('Name has to be at least 50 characters long and cannot contain special characters except dots and commas.');
     }
   }
   function handleHsChange(e) {
-    if((e.target.value < 0) || (e.target.value > 100)){
+    if ((e.target.value < 0) || (e.target.value > 100)) {
       setWarningHS('Health Score cannot be less than 0 and more than 100');
     }
-    else{
+    else {
       setHs(e.target.value);
       setWarningHS('');
     }
   }
   function handleSbsChange(e) {
-    const regex = /^((?!script).){100,}$/gm;
-    if(regex.test(e.target.value)){
-      setSbs(e.target.value);
-      setWarningSbs('');
-      if((warningName + warningHS + warningSbs + warningSummary) === '')
-      setEnableBtn(false);
-    }
-    else{
-      setWarningSbs('Steps has to be at least 100 characters long and cannot contain the word "script"');
+    const regexWords = /^((?!script).){100,}$/gm;
+    const regex = /^[\w\W]{100,}$/gm;
+    if (!regex.test(e.target.value)) {
+      setWarningSbs('Recipe steps has to be at least 100 characters long');
       setEnableBtn(true);
     }
-  }
-  function handlevegetarianChange(e) {
-    setVegetarian(!vegetarian);
-  }
-  function handleVeganChange(e) {
-    setVegan(!vegan);
-  }
-  function handleglutenFreeChange(e) {
-    setGlutenFree(!glutenFree);
-  }
-  function handledairyFreeChange(e) {
-    setDairyFree(!dairyFree);
+    else if (!regexWords.test(e.target.value)) {
+      setWarningForbiddenWords('Recipe steps cannot contain this words: "script"');
+      setEnableBtn(true);
+    }
+    else {
+      setSbs(e.target.value);
+      setWarningSbs('');
+      setWarningForbiddenWords('');
+      if ((warningName + warningSbs + warningSummary + warningForbiddenWords) === '')
+        setEnableBtn(false);
+    }
   }
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -79,18 +85,36 @@ function Create() {
   };
 
 
-
   return (
     <div className={s.body}>
       <form className={s.form} onSubmit={e => { handleSubmit(e) }}>
-        <div className={s.form_items}><span>Name: </span><span className={s.warning}>{warningName}</span><input type="text" name="name"  onChange={handleNameChange} className={s.input} /></div>
+        <div className={s.form_items}><span>Name: </span><span className={s.warning}>{warningName}</span><input type="text" name="name" onChange={handleNameChange} className={s.input} /></div>
         <div className={s.form_items}><span>Summary: </span><span className={s.warning}>{warningSummary}</span><input type="text" name="summary" onChange={handleSummaryChange} className={s.input} /></div>
         <div className={s.form_items}><span>Health Score: </span><span className={s.warning}>{warningHS}</span><input type="number" name="hs" onChange={handleHsChange} className={s.input} /></div>
-        <div className={s.form_items}><span>Step by Step: </span><span className={s.warning}>{warningSbs}</span><textarea name="sbs" cols="40" rows="5" onChange={handleSbsChange} className={s.inputSbs}></textarea>{/* <input type="text" name="sbs" value={sbs} onChange={handleSbsChange} className={s.input}/> */}</div>
-        <div className={s.div_ops}><div><span>Vegetarian</span><input type="checkbox" name="vegetarian" value={vegetarian} onChange={handlevegetarianChange} /></div>
-          <div><span>Vegan</span><input type="checkbox" name="vegan" value={vegan} onChange={handleVeganChange} /></div></div>
-        <div className={s.div_ops}><div><span>GlutenFree</span><input type="checkbox" name="glutenFree" value={glutenFree} onChange={handleglutenFreeChange} /></div>
-          <div><span>DairyFree</span><input type="checkbox" name="dairyFree" value={dairyFree} onChange={handledairyFreeChange} /></div></div>
+        <div className={s.form_items}><span>Step by Step: </span><span className={s.warning}>{warningSbs}</span><span className={s.warning}>{warningForbiddenWords}</span><textarea name="sbs" cols="40" rows="5" onChange={handleSbsChange} className={s.inputSbs}></textarea></div>
+
+        <button className={s.buttonSetDiets} onClick={() => {hidden === s.hide ? setHidden(s.show) : setHidden(s.hide)}}>Set Diets</button>
+        <div className={hidden}>
+          <div className={s.div_ops}>
+            <div className={s.column}>
+              <div><input type="checkbox" name="vegetarian" value={Vegetarian} onChange={() => setVegetarian(!Vegetarian)} /><label htmlFor='vegetarian'>Vegetarian</label></div>
+              <div><input type="checkbox" name="glutenFree" value={GlutenFree} onChange={() => setGlutenFree(!GlutenFree)} /><label htmlFor='glutenFree'>Gluten Free</label></div>
+              <div><input type="checkbox" name="Ketogenic" value={Ketogenic} onChange={() => setKetogenic(!Ketogenic)} /><label htmlFor='Ketogenic'>Ketogenic</label></div>
+              <div><input type="checkbox" name="Pescetarian" value={Pescetarian} onChange={() => setPescetarian(!Pescetarian)} /><label htmlFor='Pescetarian'>Pescetarian</label></div>
+              <div><input type="checkbox" name="Primal" value={Primal} onChange={() => setPrimal(!Primal)} /><label htmlFor='Primal'>Primal</label></div>
+              <div><input type="checkbox" name="Whole30" value={Whole30} onChange={() => setWhole30(!Whole30)} /><label htmlFor='Whole30'>Whole 30</label></div>
+            </div>
+
+            <div className={s.column}>
+              <div><input type="checkbox" name="vegan" value={Vegan} onChange={() => setVegan(!Vegan)} /><label htmlFor='vegan'>Vegan</label></div>
+              <div><input type="checkbox" name="dairyFree" value={DairyFree} onChange={() => setDairyFree(!DairyFree)} /><label htmlFor='dairyFree'>Dairy Free</label></div>
+              <div><input type="checkbox" name="LactoOvoVegetarian" value={LactoOvoVegetarian} onChange={() => setLactoOvoVegetarian(!LactoOvoVegetarian)} /><label htmlFor='LactoOvoVegetarian'>Lacto-Ovo Vegetarian</label></div>
+              <div><input type="checkbox" name="Paleo" value={Paleo} onChange={() => setPaleo(!Paleo)} /><label htmlFor='Paleo'>Paleo</label></div>
+              <div><input type="checkbox" name="LowFODMAP" value={LowFODMAP} onChange={() => setLowFODMAP(!LowFODMAP)} /><label htmlFor='LowFODMAP'>Low FODMAP</label></div>
+            </div>
+          </div>
+        </div>
+
         <input disabled={enableBtn} type="submit" value="Submit" className={s.button} />
       </form>
     </div>
