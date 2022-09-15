df448af69c9a094089dae8a2ab23d5b5eea02a9f
gon2santos <necchigonzalo@gmail.com>
2022-09-15 11:18:28 -0300

    Server handles recipes request, responds with recipes from spoonacular API or appropiate message when no recipes were found

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+require('dotenv').config();
+const fetch = (...args) =>
+	import('node-fetch').then(({default: fetch}) => fetch(...args));
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +10,16 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.get('/', (req, res) => {
+    res.send('Successful response! :P')
+})
+router.get('/recipes', (req, res) => {
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${req.query.name}&number=100&apiKey=${process.env.REACT_APP_API_KEY}&addRecipeInformation=true`)
+        .then(response => response.json())
+        .then(response => {
+            !response.results.length ? res.status(404).json({ error: 'No recipes found' }) : res.json(response);
+        });
+})
 
 
 module.exports = router;
