5a8c0345ce367058e8858d23879488021f6b273f
gon2santos <necchigonzalo@gmail.com>
2022-09-15 11:52:17 -0300

    Server responds with recipe details given its id

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,14 +10,20 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get('/', (req, res) => {
-    res.send('Successful response! :P')
-})
+
 router.get('/recipes', (req, res) => {
     fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${req.query.name}&number=100&apiKey=${process.env.REACT_APP_API_KEY}&addRecipeInformation=true`)
         .then(response => response.json())
         .then(response => {
             !response.results.length ? res.status(404).json({ error: 'No recipes found' }) : res.json(response);
+        });
+})
+
+router.get('/recipes/:idReceta', (req, res) => {
+    fetch(`https://api.spoonacular.com/recipes/${req.params.idReceta}/information?apiKey=${process.env.REACT_APP_API_KEY}`)//reemplazar por llamado al server, meter esto en la api
+        .then(response => response.json())
+        .then(response => {
+            res.json(response);
         });
 })
 
