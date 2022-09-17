0beef60468de5b23560184117cb4a33c9140e27c
gon2santos <necchigonzalo@gmail.com>
2022-09-16 22:03:47 -0300

    client now connects to server to retrieve recipe list and recipe details (instead of calling the spoonacular api directly)

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -6,7 +6,7 @@ export const TOGGLE_VIEW = "TOGGLE_VIEW";
 
 export function getRecipes(value) {
     return async function(dispatch) {
-      return fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${value}&number=100&apiKey=${process.env.REACT_APP_API_KEY}&addRecipeInformation=true`)//reemplazar por llamado al server, meter esto en la api
+      return fetch(`http://localhost:3001/recipes?name=${value}`)
         .then(response => response.json())
         .then(response => {
           dispatch({ type: GET_RECIPES, payload: response});
@@ -16,7 +16,7 @@ export function getRecipes(value) {
 
 export function getDetail(id) {
     return async function(dispatch) {
-      return fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.REACT_APP_API_KEY}`)//reemplazar por llamado al server, meter esto en la api
+      return fetch(`http://localhost:3001/recipes/${id}`)
         .then(response => response.json())
         .then(response => {
           dispatch({ type: GET_DETAIL, payload: response});
