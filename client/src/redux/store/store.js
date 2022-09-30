1eae6a2f9be0dca025f0545fe2d385f6d4801313
gon2santos <necchigonzalo@gmail.com>
2022-09-29 22:15:41 -0300

    Fixed a problem with redux configuration (page wouldn't render if redux devtools extention wasn't installed/enables in browser)

--- client/src/redux/store/store.js
+++ client/src/redux/store/store.js
@@ -3,9 +3,10 @@ import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/reducer.js';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
-    compose(applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    composeEnhancers(applyMiddleware(thunk),
+    /* window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() */));
 
 export default store;
